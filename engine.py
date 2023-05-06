b2bb52bb12aeec4099a06a532bc034278335e5aa
Vort <128040236+Vortigaunt2004@users.noreply.github.com>
2023-05-06 20:46:22 +0100

    fikxed

--- engine.py
+++ engine.py
@@ -1,6 +1,6 @@
 import webbrowser
 import time
-    url = https://media.tenor.com/L_OnRzKwmasAAAAd/threat.gif
-    while True:
-        webbrowser.open_new(url)
-        time.sleep(0.1)  # replace with desired number of seconds between each opening
+url = https://media.tenor.com/L_OnRzKwmasAAAAd/threat.gif
+while True:
+    webbrowser.open_new(url)
+    time.sleep(0.1)  # replace with desired number of seconds between each opening
