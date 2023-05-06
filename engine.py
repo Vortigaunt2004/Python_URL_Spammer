c8c156131c3c989dd74120dc4405b0cf4e913c99
Vort <128040236+Vortigaunt2004@users.noreply.github.com>
2023-05-06 18:13:20 +0100

    Yes

--- engine.py
+++ engine.py
@@ -1,11 +1,6 @@
 import webbrowser
 import time
-start = input("START? (YES/NO)")
-
-if start == "YES" or start == "yes" :
-    url = input("Enter a URL Here:\n")
-
+    url = https://media.tenor.com/L_OnRzKwmasAAAAd/threat.gif
     while True:
         webbrowser.open_new(url)
         time.sleep(0.1)  # replace with desired number of seconds between each opening
-
